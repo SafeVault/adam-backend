1d0a9927f2371ef0ce9ab612068cc3a8b1b1e80d
Bolu Adetifa <boluadetifa@gmail.com>
2025-04-29 09:47:48 +0100

    feat(app): integrate payroll into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
+import { PayrollsModule } from './payroll/payroll.module';
+import { Payroll } from './payroll/payroll.entity';
+import { PayrollEmployeeDetail } from './payroll/payroll-employee.entity';
 
 @Module({
   imports: [
@@ -11,13 +14,14 @@ import { User } from './users/user.entity';
       host: 'localhost',
       port: 5432,
       username: 'postgres',
-      password: '',
-      database: 'sv-adam-db',
-      entities: [User],
+      password: 'postgres13240!',
+      database: 'adam_backend_db',
+      entities: [User, Payroll, PayrollEmployeeDetail],
       synchronize: true, // set to false in production
     }),
     AuthModule,
     UsersModule,
+    PayrollsModule
   ],
 })
 export class AppModule {}
